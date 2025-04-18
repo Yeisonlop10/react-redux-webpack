11aac751bd09e3bafcf8f6c8f6f6aae7d2a87b64
Yeison Lopez <yeison.lopez1028@gmail.com>
2025-04-18 18:37:06 -0400

    Migrate to eslint.config.js

---
+++ eslint.config.js
@@ -0,0 +1,34 @@
+// eslint.config.js
+const js = require('@eslint/js');
+const react = require('eslint-plugin-react');
+
+module.exports = [
+  js.configs.recommended,
+  react.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+      globals: {
+        ...js.configs.recommended.globals,
+        browser: true,
+      },
+    },
+    plugins: {
+      react: react,
+    },
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  },
+];
