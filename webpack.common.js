7c4d1eb95168db030df1a05fd78a00e4848558a6
Yeison Lopez <yeison.lopez1028@gmail.com>
2025-04-18 21:09:06 -0400

    typo

--- webpack.common.js
+++ webpack.common.js
@@ -2,7 +2,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.jsx',
+  entry: './src/index.js',
   output: {
     filename: 'bundle.[contenthash].js',
     path: path.resolve(__dirname, 'dist'),
